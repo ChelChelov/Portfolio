23642ba19ba7f97520aa7bbf2d3ec651cf070339
Oleg Sazonov <sazonov1994@gmail.com>
2023-12-20 19:42:18 +0200

    add menu (html + scss)

---
+++ dist/js/script.js
@@ -0,0 +1,24 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+closeElem.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+});
+
+
+
+
+
