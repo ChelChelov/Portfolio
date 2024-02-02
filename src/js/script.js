11ede10b848f8ebe8dd8ad8ada4f1f6317982cb1
Oleg Sazonov <sazonov1994@gmail.com>
2024-02-02 19:07:55 +0200

    add pageup (html + scss); add smooth scrolling + pageup scrolling (js)

--- src/js/script.js
+++ src/js/script.js
@@ -24,3 +24,47 @@ const percents = document.querySelectorAll('.skills__rate-percent'),
 percents.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('.menu__list a[href^="#"]')
+    const scrollToTopButton = document.getElementById('scrollToTop')
+    const pageup = document.querySelector('.pageup');
+
+    //Smooth scrolling
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+
+    //Pageup scrolling
+    scrollToTopButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+
+    window.addEventListener('scroll', function() {
+        const documentHeight = document.body.scrollHeight;
+        const scrollStart = 1600;
+        const scrollEnd = documentHeight - 1300;
+        const scrollPosition = window.scrollY;
+
+        if (scrollPosition > scrollStart && scrollPosition < scrollEnd) {
+            pageup.style.display = 'block';
+        } else {
+            pageup.style.display = 'none';
+        }
+    });
+});
